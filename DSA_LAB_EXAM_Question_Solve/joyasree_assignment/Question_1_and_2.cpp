31c10d4bae9699163ec5ff3339ff3d6c09dabaa3
Tashin Parvez <tashinparvez2002@gmail.com>
2023-07-23 21:50:40 +0600

    add comments on question 1 and 2

--- DSA_LAB_EXAM_Question_Solve/joyasree_assignment/Question_1_and_2.cpp
+++ DSA_LAB_EXAM_Question_Solve/joyasree_assignment/Question_1_and_2.cpp
@@ -11,79 +11,87 @@ public:
 };
 
 Node *LL_insert_to_beginning(Node *head, int newData)
-{ // needs assignment
-    Node *newNode = new Node();
-    newNode->data = newData;
-    newNode->next = head;
-    head->prev = newNode;
-    newNode->prev = NULL;
+{
+
+    Node *newNode = new Node(); // creat new node
+    newNode->data = newData;    // assign value to the new node
+
+    newNode->prev = NULL; // this is a double ll so to set new node as head its head prev should be null
+    newNode->next = head; // new node er next e head point kora
+    head->prev = newNode; // ar jehetu head er aage new node bosche so head er prev e new node assign korechi
+
     return newNode;
 }
 
 void LL_insert_to_beginning(Node **head, int newData)
-{ // One without assignment needed
-    Node *newNode = new Node();
-    Node *temp = *head;
-    newNode->data = newData;
-    newNode->prev = NULL;
-    newNode->next = temp;
-    temp->prev = newNode;
+{                               // One without assignment needed
+    Node *newNode = new Node(); // creat new node
+    newNode->data = newData;    // assign value to the new node
+
+    Node *temp = *head;   // head er ekta copy node banano
+    newNode->prev = NULL; // this is a double ll so to set new node as head its head prev should be null
+    newNode->next = temp; // new node er next e head point kora
+    temp->prev = newNode; // ar jehetu head er aage new node bosche so head er prev e new node assign korechi
     *head = newNode;
 }
 
 void LL_insert_after(Node *head, int newData, int target)
 {
-    Node *temp = head;
+    Node *temp = head; // head er ekta copy node banano
     while (temp->next != NULL && temp->data != target)
     {
-        temp = temp->next;
+        temp = temp->next; // to find the target node
     }
-    Node *newNode = new Node();
-    newNode->data = newData;
 
-    newNode->next = temp->next;
-    temp->next->prev = newNode;
-    temp->next = newNode;
-    newNode->prev = temp;
+    Node *newNode = new Node(); // creat new node
+    newNode->data = newData;    // assign value to the new node
+
+    newNode->next = temp->next; // new node er next e je target node tar poer node assign
+    temp->next->prev = newNode; // target node er porer noder er prev e new node assign
+    temp->next = newNode;       // target node er next e new node assigh
+    newNode->prev = temp;       // sesh s new node er aage target node assign
 }
 
 void LL_insert_at_end(Node *head, int newData)
 {
-    Node *temp = head;
+    Node *temp = head; // head er ekta copy node banano
     while (temp->next != NULL)
     {
-        temp = temp->next;
+        temp = temp->next; // sesh node e powchano
     }
-    Node *newNode = new Node();
-    newNode->data = newData;
-    temp->next = newNode;
-    newNode->prev = temp;
+    Node *newNode = new Node(); // creat new node
+    newNode->data = newData;    // assign value to the new node
+
+    temp->next = newNode; // sesh node er pore new node ke point kora
+    newNode->prev = temp; // sesh node er aage aager last node ke point kora
+    newNode->next = NULL; // new node jeetu last node so er next e null pioint kora
 }
 
 void LL_delete_node(Node **head, int hasValue)
 {
-    Node *temp = *head;
-    if (temp->data == hasValue)
+    Node *temp = *head;         // head er ekta copy node banano
+    if (temp->data == hasValue) // node jodi prothom node hoy
     {
-        *head = temp->next;
-        Node *first = *head;
-        first->prev = NULL;
-        free(temp);
+        *head = temp->next;  //  head sorano karon head e remove hobe
+        Node *first = *head; // new head er copy node banano
+        first->prev = NULL;  // head er aage null point kora
+        free(temp);          // node free kora
         return;
     }
-    Node *prevNode;
+
+    Node *prevNode; // je node delete hobe setar aager node jate easily pete pari tar jonno eta
     while (temp != NULL && temp->data != hasValue)
     {
-        prevNode = temp;
-        temp = temp->next;
+        prevNode = temp;   // previous node e current node assign
+        temp = temp->next; // current node ne porer node e assign
     }
     if (temp == NULL)
     {
         cout << "The item doesn't exist \n";
         return;
     }
-    prevNode->next = temp->next;
-    temp->next->prev = prevNode;
+    prevNode->next = temp->next; // prev node ke je node delete hobe tar poer node ke point kora
+    temp->next->prev = prevNode; // je node delete hobe tar porer node er prev ke je node delete hobe tar aager node ke point kora
     free(temp);
 }
 
