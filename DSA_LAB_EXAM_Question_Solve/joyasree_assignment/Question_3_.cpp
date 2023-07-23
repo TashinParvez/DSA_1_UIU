0d455f1db4f816d383911592f84f9b66286ccc27
Tashin Parvez <tashinparvez2002@gmail.com>
2023-07-23 15:33:35 +0600

    Friends Assignment 3 done

--- DSA_LAB_EXAM_Question_Solve/joyasree_assignment/Question_3_.cpp
+++ DSA_LAB_EXAM_Question_Solve/joyasree_assignment/Question_3_.cpp
@@ -15,17 +15,18 @@ Node *LL_insert_to_beginning(Node *head, int newData)
     Node *newNode = new Node();
     newNode->data = newData;
     newNode->next = head;
+    newNode->prev = head->prev;
     head->prev = newNode;
-    newNode->prev = NULL;
     return newNode;
 }
 
 void LL_insert_to_beginning(Node **head, int newData)
 { // One without assignment needed
     Node *newNode = new Node();
-    Node *temp = *head;
     newNode->data = newData;
-    newNode->prev = NULL;
+    Node *temp = *head;
+
+    newNode->prev = temp->prev;
     newNode->next = temp;
     temp->prev = newNode;
     *head = newNode;
@@ -34,7 +35,7 @@ void LL_insert_to_beginning(Node **head, int newData)
 void LL_insert_after(Node *head, int newData, int target)
 {
     Node *temp = head;
-    while (temp->next != NULL && temp->data != target)
+    while (temp->next != head && temp->data != target)
     {
         temp = temp->next;
     }
@@ -50,12 +51,13 @@ void LL_insert_after(Node *head, int newData, int target)
 void LL_insert_at_end(Node *head, int newData)
 {
     Node *temp = head;
-    while (temp->next != NULL)
+    while (temp->next != head)
     {
         temp = temp->next;
     }
     Node *newNode = new Node();
     newNode->data = newData;
+    newNode->next = temp->next;
     temp->next = newNode;
     newNode->prev = temp;
 }
@@ -65,19 +67,20 @@ void LL_delete_node(Node **head, int hasValue)
     Node *temp = *head;
     if (temp->data == hasValue)
     {
+        temp->next->prev = temp->prev;
+        temp->prev->next = temp->next;
         *head = temp->next;
-        Node *first = *head;
-        first->prev = NULL;
         free(temp);
         return;
     }
+    temp = temp->next;
     Node *prevNode;
-    while (temp != NULL && temp->data != hasValue)
+    while (temp != *head && temp->data != hasValue)
     {
         prevNode = temp;
         temp = temp->next;
     }
-    if (temp == NULL)
+    if (temp == *head)
     {
         cout << "The item doesn't exist \n";
         return;
@@ -87,11 +90,13 @@ void LL_delete_node(Node **head, int hasValue)
     free(temp);
 }
 
-bool searchngitems(Node *head, int data)  
+bool searchngitems(Node *head, int data)
 {
     Node *traveller = head;
-    while (traveller->next != NULL)
-
+    if (traveller->data == data)
+        return true;
+    traveller = traveller->next;
+    while (traveller->next != head)
     {
         if (traveller->data == data)
             return true;
