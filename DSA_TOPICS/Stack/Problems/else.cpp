87af6c21c534a127ab7b5f961dea70ac06aafc26
Tashin Parvez <tashinparvez2002@gmail.com>
2023-08-06 02:30:12 +0600

    Prep for Online

---
+++ DSA_TOPICS/Stack/Problems/else.cpp
@@ -0,0 +1,13 @@
+//        ****************  Author :  Tashin.Parvez  *************************\
+//        ****************  Updated:    22-06-23     *************************\
+
+#include <bits/stdc++.h>
+#define    CRACKED   return 0;
+#define    nl        endl;              // NewLine
+using  namespace std;
+
+int32_t main()
+{   
+    
+    CRACKED;
+}
