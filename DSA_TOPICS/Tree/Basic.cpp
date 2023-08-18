fffccef93d409c06120460eaf6a6114115e7b2ee
Tashin Parvez <tashinparvez2002@gmail.com>
2023-08-18 12:42:35 +0600

    Tree Basic

---
+++ DSA_TOPICS/Tree/Basic.cpp
@@ -0,0 +1,53 @@
+//        ****************  Author :  Tashin.Parvez  *************************\
+//        ****************  Updated:    22-06-23     *************************\
+
+#include <bits/stdc++.h>
+#define CRACKED return 0;
+#define nl endl; // NewLine
+#define null NULL
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *leftNode;
+    node *rightNode;
+    node(int data)
+    {
+        this->data = data;
+        leftNode = NULL;
+        rightNode = NULL;
+    }
+};
+
+node *buildTree(node *root)
+{
+    cout << "Enter the value of root: ";
+    int data;
+    cin >> data;
+    root = new node(data);
+
+    if (data == -1)
+    {
+        return NULL;
+    }
+
+    cout << "Enter  left child of " << data << " " << nl;
+    root->leftNode = buildTree(root->leftNode);
+
+    cout << "Enter  right child of " << data << " " << nl;
+    root->rightNode = buildTree(root->rightNode);
+
+    return root;
+}
+
+int32_t main()
+{
+    node *root = NULL;
+
+    // creating tree
+    root = buildTree(root);
+
+    CRACKED;
+}
---
+++ DSA_TOPICS/Tree/Tree_PreOrder.cpp
@@ -0,0 +1,14 @@
+//        ****************  Author :  Tashin.Parvez  *************************\
+//        ****************  Updated:    22-06-23     *************************\
+
+#include <bits/stdc++.h>
+#define CRACKED return 0;
+#define nl endl; // NewLine
+#define null NULL
+using namespace std;
+
+int32_t main()
+{
+
+    CRACKED;
+}
